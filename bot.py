d59f795cb834ad1e848d3a879338c2d6fd6dd502
Ilashlm10 <ilashadam3@gmail.com>
2024-07-25 00:51:21 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,52 +1,55 @@
 import logging
 import logging.config
+import uvloop
+from typing import Optional, Union, AsyncGenerator
 
 # Get logging configurations
-logging.config.fileConfig('logging.conf')
+logging.config.fileConfig("logging.conf")
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
-from pyrogram import Client, __version__
+
+from pyrogram import Client, __version__, types
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
-from utils import temp
-from typing import Union, Optional, AsyncGenerator
-from pyrogram import types
+from utils import temp, scheduler
+import asyncio
+
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
             name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
+            workers=1000,
             plugins={"root": "plugins"},
-            sleep_threshold=5,
+            sleep_threshold=2,
         )
 
     async def start(self):
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats
         await super().start()
+        scheduler.start()
         await Media.ensure_indexes()
-        me = await self.get_me()
-        temp.ME = me.id
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
-        self.username = '@' + me.username
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        self.me = await self.get_me()
+        temp.ME = self.me.id
+        temp.U_NAME = self.me.username
+        temp.B_NAME = self.me.first_name
+        self.username = "@" + self.me.username
+        logging.info(
+            f"{self.me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {self.me.username}."
+        )
         logging.info(LOG_STR)
 
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped. Bye.")
-    
+
     async def iter_messages(
         self,
         chat_id: Union[int, str],
@@ -62,10 +65,10 @@ class Bot(Client):
                 Unique identifier (int) or username (str) of the target chat.
                 For your personal cloud (Saved Messages) you can simply use "me" or "self".
                 For a contact that exists in your Telegram address book you can use his phone number (str).
-                
+
             limit (``int``):
                 Identifier of the last message to be returned.
-                
+
             offset (``int``, *optional*):
                 Identifier of the first message to be returned.
                 Defaults to 0.
@@ -81,7 +84,9 @@ class Bot(Client):
             new_diff = min(200, limit - current)
             if new_diff <= 0:
                 return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            messages = await self.get_messages(
+                chat_id, list(range(current, current + new_diff + 1))
+            )
             for message in messages:
                 yield message
                 current += 1
